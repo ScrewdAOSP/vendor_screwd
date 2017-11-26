a9431bd251907b15c9dafbb8ad059d34f6fb75c8
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-11-26 03:10:20 +0100

    op2: Kill CPU sets
    Task designation works perfectly fine without any of this fuckery...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,9 +123,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 #BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
-# Cpusets
-ENABLE_CPUSETS := true
-
 # Filesystem
 TARGET_FS_CONFIG := $(PLATFORM_PATH)/config.fs
 
