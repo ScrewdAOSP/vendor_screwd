481e4c7320e817b18f0ecac78e70912227976375
RJ Trujillo <CertifiedBlyndGuy@gmail.com>
2017-12-03 18:38:10 +0100

    op2: camera: Clang for everyone
    Times are a-changin

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -53,7 +53,6 @@ LOCAL_SRC_FILES := \
     src/mm_jpegdec_interface.c \
     src/mm_jpegdec.c
 
-LOCAL_CLANG            := false
 LOCAL_MODULE           := libmmjpeg_interface
 LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := liblog libdl libcutils liblog libqomx_core
